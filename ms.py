940490cb39494ce89cb30cd78f2891179423667f
Heesu <heesu@nhnnext.org>
2013-12-26 16:29:39 +0900

    Working correctly now, thanks to 희수터디.

--- ms.py
+++ ms.py
@@ -1,74 +1,46 @@
 # -*- coding:utf-8 -*-
 import random
-
-# 실패 : #shuffle 실패 & 4칸만 지뢰 형성되는 게 아님ㅠ  #입력하면 한 '열'이 다 열림
+from sys import stdout
+# 실패1 : #shuffle 실패 & 4칸만 지뢰 형성되는 게 아님ㅠ  #입력하면 한 '열'이 다 열림
 
 n=20
-pz=[[0]*n]*n
-ans=[[' ']*n]*n
+making=[0]*n*n #실패1해결.
+pz = []
+ans=[]
+for i in range(n) :
+	ans.append([' ']*n)
 
 def gen_puzzle():
 	#16%만큼 지뢰(*)형성
-	for i in range (int(n*0.4)):
-		for j in range (int(n*0.4)):
-			pz[i][j]='*'
-	random.shuffle(pz)
-	#shuffle 실패 & 4칸만 지뢰 형성되는 게 아님ㅠ
+	for i in range (int(n*0.4)**2):
+		making[i]='*'
+	random.shuffle(making)
+
 	for i in range(n):
-		for j in range(n):
+		pz.append(making[n*i:n*i+n])
+
+	for i in range(n) :
+		for j in range(n) :
 			if pz[i][j]==0:
 				chk_around(i,j)
+"""
+	for i in range(n):
+		for j in range(n) :
+			stdout.write("%3s" % pz[i][j])
+		print("")
+"""
 
 def chk_around(x,y):
 	#여기가 짜증남, 일일이 범위 나누고 체크 일일이 써줘야 하나?
-	if x==0:
-		if y==0:
-			for i in range (x,x+2):
-				for j in range (y,y+2):
-					if pz[i][j]=='*':
-						pz[x][y]+=1
-		elif y==n-1:
-			for i in range (x,x+2):
-				for j in range (y-1,y+1):
-					if pz[i][j]=='*':
-						pz[x][y]+=1
-		else:
-			for i in range (x,x+2):
-				for j in range (y-1,y+2):
-					if pz[i][j]=='*':
-						pz[x][y]+=1
-	elif x==n-1:
-		if y==0:
-			for i in range (x-1,x+1):
-				for j in range (y,y+2):
-					if pz[i][j]=='*':
-						pz[x][y]+=1
-		elif y==n-1:
-			for i in range (x-1,x+1):
-				for j in range (y-1,y+1):
-					if pz[i][j]=='*':
-						pz[x][y]+=1
-		else:
-			for i in range (x-1,x+1):
-				for j in range (y-1,y+2):
-					if pz[i][j]=='*':
-						pz[x][y]+=1
-	else:
-		if y==0:
-			for i in range (x-1,x+2):
-				for j in range (y,y+2):
-					if pz[i][j]=='*':
-						pz[x][y]+=1
-		elif y==n-1:
-			for i in range (x-1,x+2):
-				for j in range (y-1,y+1):
-					if pz[i][j]=='*':
-						pz[x][y]+=1
-		else:
-			for i in range (x-1,x+2):
-				for j in range (y-1,y+2):
-					if pz[i][j]=='*':
-						pz[x][y]+=1
+	for i in range(-1,2) :
+		if (x+i < 0) or (x+i >= n) :
+			continue
+		for j in range(-1,2) :
+			if (y+j < 0) or (y+j >= n) :
+				continue
+			if pz[x+i][y+j]=='*':
+				pz[x][y]+=1
+
 
 def prt_puzzle():
 	for i in range (n):
@@ -80,22 +52,27 @@ def prt_puzzle():
 		print "|"
 	for j in range (n):
 		print " - ",
+	print "\n"
 
 def guessing(x,y):
 	if pz[x][y]=='*':
+		ans[x][y]=pz[x][y]
 		return 1
 	else:
 		ans[x][y]=pz[x][y]
 		return 0
 	#지뢰면 1반환, 아니면 화면에 주변지뢰개수를 출력하고 0반환
 
+gen_puzzle()
+
 # (입력받고, 지뢰인지 확인하고, 주변지뢰수 출력)을 (전체칸-지뢰수)만큼
 # 구현 못 함ㅠㅠ : 주변지뢰수가 0일때 주변8칸 다열여주고 for문의 횟수 8번 줄이기 (tr+=8해보려했으나 다음 반복에서 다시 다음수로 넘어감)
-for tr in range (n*n-int(n*0.16)):
+for tr in range ((n*n)-int(n*0.16)):
 	prt_puzzle()
 	gss=input("입력 (ex-0,0) : ")
 	bmb=guessing(gss[0],gss[1])
 	if bmb:
+		prt_puzzle()
 		print "Game Over!"
 		break
 	
